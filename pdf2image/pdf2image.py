c73e12f3bba90eda392d198a4e0a617adb3492c2
Tim Guo <Tim.Guo@transport.nsw.gov.au>
2024-08-08 10:09:25 +1000

    fixed save bug

--- pdf2image/pdf2image.py
+++ pdf2image/pdf2image.py
@@ -59,62 +59,14 @@ def convert_from_path(
     timeout: int = None,
     hide_annotations: bool = False,
 ) -> List[Image.Image]:
-    """Function wrapping pdftoppm and pdftocairo
-
-    :param pdf_path: Path to the PDF that you want to convert
-    :type pdf_path: Union[str, PurePath]
-    :param dpi: Image quality in DPI (default 200), defaults to 200
-    :type dpi: int, optional
-    :param output_folder: Write the resulting images to a folder (instead of directly in memory), defaults to None
-    :type output_folder: Union[str, PurePath], optional
-    :param first_page: First page to process, defaults to None
-    :type first_page: int, optional
-    :param last_page: Last page to process before stopping, defaults to None
-    :type last_page: int, optional
-    :param fmt: Output image format, defaults to "ppm"
-    :type fmt: str, optional
-    :param jpegopt: jpeg options `quality`, `progressive`, and `optimize` (only for jpeg format), defaults to None
-    :type jpegopt: Dict, optional
-    :param thread_count: How many threads we are allowed to spawn for processing, defaults to 1
-    :type thread_count: int, optional
-    :param userpw: PDF's password, defaults to None
-    :type userpw: str, optional
-    :param ownerpw: PDF's owner password, defaults to None
-    :type ownerpw: str, optional
-    :param use_cropbox: Use cropbox instead of mediabox, defaults to False
-    :type use_cropbox: bool, optional
-    :param strict: When a Syntax Error is thrown, it will be raised as an Exception, defaults to False
-    :type strict: bool, optional
-    :param transparent: Output with a transparent background instead of a white one, defaults to False
-    :type transparent: bool, optional
-    :param single_file: Uses the -singlefile option from pdftoppm/pdftocairo, defaults to False
-    :type single_file: bool, optional
-    :param output_file: What is the output filename or generator, defaults to uuid_generator()
-    :type output_file: Any, optional
-    :param poppler_path: Path to look for poppler binaries, defaults to None
-    :type poppler_path: Union[str, PurePath], optional
-    :param grayscale: Output grayscale image(s), defaults to False
-    :type grayscale: bool, optional
-    :param size: Size of the resulting image(s), uses the Pillow (width, height) standard, defaults to None
-    :type size: Union[Tuple, int], optional
-    :param paths_only: Don't load image(s), return paths instead (requires output_folder), defaults to False
-    :type paths_only: bool, optional
-    :param use_pdftocairo: Use pdftocairo instead of pdftoppm, may help performance, defaults to False
-    :type use_pdftocairo: bool, optional
-    :param timeout: Raise PDFPopplerTimeoutError after the given time, defaults to None
-    :type timeout: int, optional
-    :param hide_annotations: Hide PDF annotations in the output, defaults to False
-    :type hide_annotations: bool, optional
-    :raises NotImplementedError: Raised when conflicting parameters are given (hide_annotations for pdftocairo)
-    :raises PDFPopplerTimeoutError: Raised after the timeout for the image processing is exceeded
-    :raises PDFSyntaxError: Raised if there is a syntax error in the PDF and strict=True
-    :return: A list of Pillow images, one for each page between first_page and last_page
-    :rtype: List[Image.Image]
-    """
-
     # Open the PDF file
     pdf_document = fitz.open(pdf_path)
 
+    auto_temp_dir = False
+    if output_folder is None:
+        output_folder = tempfile.mkdtemp()
+        auto_temp_dir = True
+        
     # Iterate through each page
     for page_num in range(len(pdf_document)):
         page = pdf_document.load_page(page_num)
@@ -122,14 +74,10 @@ def convert_from_path(
         
         # Save the image
         output_path = os.path.join(output_folder, f'page_{page_num + 1}.png')
-        # pix.save(output_path)
+        pix.save(output_path)
 
     images = []
 
-    auto_temp_dir = False
-    if output_folder is None:
-        output_folder = tempfile.mkdtemp()
-        auto_temp_dir = True
         
     if output_folder is not None:
         images += _load_from_output_folder(
@@ -139,6 +87,8 @@ def convert_from_path(
             paths_only,
             in_memory=auto_temp_dir,
         )
+        
+    return images
 
 
 def convert_from_bytes(
@@ -165,59 +115,6 @@ def convert_from_bytes(
     timeout: int = None,
     hide_annotations: bool = False,
 ) -> List[Image.Image]:
-    """Function wrapping pdftoppm and pdftocairo.
-
-    :param pdf_bytes: Bytes of the PDF that you want to convert
-    :type pdf_bytes: bytes
-    :param dpi: Image quality in DPI (default 200), defaults to 200
-    :type dpi: int, optional
-    :param output_folder: Write the resulting images to a folder (instead of directly in memory), defaults to None
-    :type output_folder: Union[str, PurePath], optional
-    :param first_page: First page to process, defaults to None
-    :type first_page: int, optional
-    :param last_page: Last page to process before stopping, defaults to None
-    :type last_page: int, optional
-    :param fmt: Output image format, defaults to "ppm"
-    :type fmt: str, optional
-    :param jpegopt: jpeg options `quality`, `progressive`, and `optimize` (only for jpeg format), defaults to None
-    :type jpegopt: Dict, optional
-    :param thread_count: How many threads we are allowed to spawn for processing, defaults to 1
-    :type thread_count: int, optional
-    :param userpw: PDF's password, defaults to None
-    :type userpw: str, optional
-    :param ownerpw: PDF's owner password, defaults to None
-    :type ownerpw: str, optional
-    :param use_cropbox: Use cropbox instead of mediabox, defaults to False
-    :type use_cropbox: bool, optional
-    :param strict: When a Syntax Error is thrown, it will be raised as an Exception, defaults to False
-    :type strict: bool, optional
-    :param transparent: Output with a transparent background instead of a white one, defaults to False
-    :type transparent: bool, optional
-    :param single_file: Uses the -singlefile option from pdftoppm/pdftocairo, defaults to False
-    :type single_file: bool, optional
-    :param output_file: What is the output filename or generator, defaults to uuid_generator()
-    :type output_file: Any, optional
-    :param poppler_path: Path to look for poppler binaries, defaults to None
-    :type poppler_path: Union[str, PurePath], optional
-    :param grayscale: Output grayscale image(s), defaults to False
-    :type grayscale: bool, optional
-    :param size: Size of the resulting image(s), uses the Pillow (width, height) standard, defaults to None
-    :type size: Union[Tuple, int], optional
-    :param paths_only: Don't load image(s), return paths instead (requires output_folder), defaults to False
-    :type paths_only: bool, optional
-    :param use_pdftocairo: Use pdftocairo instead of pdftoppm, may help performance, defaults to False
-    :type use_pdftocairo: bool, optional
-    :param timeout: Raise PDFPopplerTimeoutError after the given time, defaults to None
-    :type timeout: int, optional
-    :param hide_annotations: Hide PDF annotations in the output, defaults to False
-    :type hide_annotations: bool, optional
-    :raises NotImplementedError: Raised when conflicting parameters are given (hide_annotations for pdftocairo)
-    :raises PDFPopplerTimeoutError: Raised after the timeout for the image processing is exceeded
-    :raises PDFSyntaxError: Raised if there is a syntax error in the PDF and strict=True
-    :return: A list of Pillow images, one for each page between first_page and last_page
-    :rtype: List[Image.Image]
-    """
-
     fh, temp_filename = tempfile.mkstemp()
     try:
         with open(temp_filename, "wb") as f:
@@ -331,27 +228,6 @@ def pdfinfo_from_bytes(
     first_page: int = None,
     last_page: int = None,
 ) -> Dict:
-    """Function wrapping poppler's pdfinfo utility and returns the result as a dictionary.
-
-    :param pdf_bytes: Bytes of the PDF that you want to convert
-    :type pdf_bytes: bytes
-    :param userpw: PDF's password, defaults to None
-    :type userpw: str, optional
-    :param ownerpw: PDF's owner password, defaults to None
-    :type ownerpw: str, optional
-    :param poppler_path: Path to look for poppler binaries, defaults to None
-    :type poppler_path: Union[str, PurePath], optional
-    :param rawdates: Return the undecoded data strings, defaults to False
-    :type rawdates: bool, optional
-    :param timeout: Raise PDFPopplerTimeoutError after the given time, defaults to None
-    :type timeout: int, optional
-    :param first_page: First page to process, defaults to None
-    :type first_page: int, optional
-    :param last_page: Last page to process before stopping, defaults to None
-    :type last_page: int, optional
-    :return: Dictionary containing various information on the PDF
-    :rtype: Dict
-    """
     from pdfminer.pdfparser import PDFParser
     from pdfminer.pdfdocument import PDFDocument
     from pdfminer.pdfinterp import resolve1
